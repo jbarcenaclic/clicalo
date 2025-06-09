726a6ef7898b636e9747ca4abd61bb6da2edef6d
JB Clicalo <jbarcena@clicalo.com>
2025-06-09 14:48:55 -0500

    Add Jest test for formatCurrency

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+};
